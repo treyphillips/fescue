6bcbef6b6972d4ed69dc0d55873f2c99ddb1e99c
Trey Phillips <tlphillipsjr@gmail.com>
2015-04-01 20:15:03 -0400

    ready for github pages. Loading page gif fixed

---
+++ app/views/toggle-switch.js
@@ -0,0 +1,19 @@
+// import Ember from 'ember';
+//
+// App.ToggleSwitch = Ember.View.extend({
+//   classNames: ['toggle-switch'],
+//   templateName: 'toggle_switch',
+//
+//   init: function() {
+//     this._super.apply(this, arguments);
+//     return this.on('change', this, this._updateElementValue);
+//   },
+//
+//   checkBoxId: (function() {
+//     return "checker-" + (this.get('elementId'));
+//   }).property(),
+//
+//   _updateElementValue: function() {
+//     return this.set('checked', this.$('input').prop('checked'));
+//   }
+// });
